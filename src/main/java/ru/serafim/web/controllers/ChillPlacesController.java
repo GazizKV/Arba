14ac24eb2a54e96902a7d81bb2748f8d51c24365
ValitovGaziz <valitovgaziz@gmail.com>
2021-11-15 12:46:08 +0500

    Save. Stop on implementing PostMapping into ChillPlacesController

--- src/main/java/ru/serafim/web/controllers/ChillPlacesController.java
+++ src/main/java/ru/serafim/web/controllers/ChillPlacesController.java
@@ -1,12 +1,12 @@
 package ru.serafim.web.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.context.properties.source.ConfigurationPropertyName;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.serafim.web.dto.ChillPlaceDto;
 import ru.serafim.web.services.ChillPlacesService;
 
 @Controller
@@ -19,13 +19,16 @@ public class ChillPlacesController {
     @GetMapping
     public String getRests(Model model) {
         model.addAttribute("chillPlaces", chillPlacesService.getAllRestPlases());
+        model.addAttribute("chillPlaceDto", new ChillPlaceDto());
         return "chillPlaces";
     }
 
     @PostMapping("/insertIntoDataBaseNewChillPlace")
-    public String inserNewChillPlace(Model model) {
+    public String inserNewChillPlace(ChillPlaceDto chillPlaceDto, Model model) {
         int i = 0;
+        String name = chillPlaceDto.getName();
+        model.addAttribute("chillPlaces", chillPlacesService.getAllRestPlases());
+        model.addAttribute("chillPlaceDto", new ChillPlaceDto());
         return "/chillPlaces";
     }
-
 }
