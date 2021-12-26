d4e059179693220cbf3126d8fdc256e97db6bc8e
gazizkv <valitovgaziz@gmail.com>
2021-12-26 12:47:07 +0500

    Добавил вывод адресов и телефонов мест отдыха на голавной странице.

--- src/main/java/ru/serafim/web/models/TypeOfRest.java
+++ src/main/java/ru/serafim/web/models/TypeOfRest.java
@@ -11,6 +11,9 @@ import javax.persistence.Entity;
 public enum TypeOfRest {
     MONUMENT, PARK, FOUNTAIN, FAMOUS_BUILDING, THEATRE, MUSEUM, RELIGIOUS_STRUCTURES
 
-    // TODO добавить все эти типы отды в класс аккаунт вместе с репозиторием для иконок, сервисом для добавления и страницей для добавления?
+    // TODO добавить все эти типы отдыха в класс Account
+    //  вместе с репозиторием для иконок,
+    //  сервисом для добавления и
+    //  страницей для добавления?
 
 }
