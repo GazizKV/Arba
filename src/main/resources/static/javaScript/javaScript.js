a654d0841abb6d28bfffedb73bc587d8e8f77298
gazizkv <valitovgaziz@gmail.com>
2021-12-27 20:16:30 +0500

    Добавил немного TODO шек)))

--- src/main/resources/static/javaScript/javaScript.js
+++ src/main/resources/static/javaScript/javaScript.js
@@ -1,3 +1,8 @@
 window.onload = function () {
-
+    console.log(document.getElementById("role").innerText)
+    if(document.getElementById("role").innerText === "ADMIN") {
+        console.log(document.getElementById("adminFunctions").getAttribute("visibility"));
+        document.getElementById("adminFunctions").setAttribute("visibility", "visible");
+        console.log(document.getElementById("adminFunctions").getAttribute("visibility"));
+    }
 }
